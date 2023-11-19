5f0ad2f61f8818bddfb7e51a7188388eccd14e0c
Yifan Liu <123603507+YifanLiu2@users.noreply.github.com>
2023-11-18 22:23:35 -0500

    Add files via upload

---
+++ text_data_preprocessing.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
+
+
+def plot_svd_performance(texts, n_components):
+    """
+    Plot the explained variance ratio vs. n_components.
+
+    Parameters:
+    texts (list of str): The list of texts to be processed.
+    """
+    vectorizor = TfidfVectorizer()
+    tfidf_matrix = vectorizor.fit_transform(texts)
+
+    svd = TruncatedSVD(n_components=n_components, random_state=42)
+    svd.fit(tfidf_matrix)
+    plt.figure(figsize=(10,6))
+    plt.plot(range(1, len(svd.explained_variance_ratio_) + 1),
+             np.cumsum(svd.explained_variance_ratio_), marker='o')
+    plt.show()
+
+
+def texts_vectorization(texts, n_components=100):
+    """
+    Preprocess the text data: Vectorize using TF-IDF and reduce dimensionality.
+
+    Parameters:
+    texts (list of str): The list of texts to be processed.
+    n_components (int): The number of dimensions to reduce to using TruncatedSVD.
+
+    Returns:
+    array: The reduced dimensionality array of the TF-IDF vectors.
+    """
+    vectorizor = TfidfVectorizer()
+    tfidf_matrix = vectorizor.fit_transform(texts)
+
+    svd = TruncatedSVD(n_components=n_components, random_state=42)
+    reduced_data = svd.fit_transform(tfidf_matrix)
+
+    return reduced_data
+
+if __name__ == "__main__":
+    df_all = pd.read_csv('../data/all.csv')
+    plot_svd_performance(df_all.text, 500)
